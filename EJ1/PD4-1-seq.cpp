8c2b57323ef8b59182f8e578c79f24aba5106f38
vostolaza <victor.ostolaza@utec.edu.pe>
2022-05-06 17:43:42 -0500

    Ejercicio 1

---
+++ EJ1/PD4-1-seq.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <omp.h>
+#include <ctime>
+using namespace std;
+
+#define SIZE 500
+
+void normaliza(double A[SIZE][SIZE]){
+    int i,j;
+    double suma=0.0, factor;
+    for (i=0; i < SIZE; i++) {
+        for (j=0; j < SIZE; j++) {
+            suma = suma + A[i][j] * A[i][j];
+        }
+    }
+    factor = 1.0/sqrt(suma);
+    for (i=0; i < SIZE; i++) {
+        for (j=0; j < SIZE; j++) {
+            A[i][j] = factor*A[i][j];
+        }
+    }
+}
+
+void print(double A[SIZE][SIZE]){
+    int i,j;
+    for (i=0; i < SIZE; i++) {
+        for (j=0; j < SIZE; j++) {
+            cout << A[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    double A[SIZE][SIZE];
+    int i,j;
+    for (i=0; i < SIZE; i++) {
+        for (j=0; j < SIZE; j++) {
+            A[i][j] = i*j;
+        }
+    }
+    // print(A);
+    clock_t start = clock();
+    normaliza(A);
+    clock_t end = clock();
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << "Tiempo de ejecucion: " << time << " segundos" << endl;
+    // print(A);
+
+    return 0;
+}
